e9e646e88711df6f4aed00b0c08ef994bff2e514
Subhajit Roy <me.subhajitroy1999@gmail.com>
2025-10-24 06:46:57 +0000

    sure try

--- src/firebase/server-init.ts
+++ src/firebase/server-init.ts
@@ -12,11 +12,14 @@ let firestore: Firestore;
 const projectId = 'studio-7539869871-a3f37';
 
 async function initializeFirebaseAdmin() {
+  // Check if there are no initialized apps
   if (!getApps().length) {
     // When running in a Google Cloud environment, the SDK will automatically
-    // use the project's service account credentials.
+    // use the project's service account credentials. Explicitly passing the 
+    // projectId ensures it always connects to the correct project.
     app = initializeApp({ projectId });
   } else {
+    // If an app is already initialized, get that instance.
     app = getApps()[0];
   }
 
